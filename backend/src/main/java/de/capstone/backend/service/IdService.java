a7512a8d389b9dbad67dceaade99e5113030e031
Kolja Willmann <93048431+Ollekmc@users.noreply.github.com>
2022-12-19 11:08:50 +0100

    Setup Models and UserController

--- backend/src/main/java/de/capstone/backend/service/IdService.java
+++ backend/src/main/java/de/capstone/backend/service/IdService.java
@@ -1,7 +1,10 @@
 package de.capstone.backend.service;
 
+import org.springframework.stereotype.Service;
+
 import java.util.UUID;
 
+@Service
 public class IdService {
 
     public String generateId() {
--- backend/src/main/java/de/capstone/backend/service/UserService.java
+++ backend/src/main/java/de/capstone/backend/service/UserService.java
@@ -3,11 +3,13 @@ package de.capstone.backend.service;
 import de.capstone.backend.model.User;
 import de.capstone.backend.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserService {
 
     private final UserRepo userRepo;
@@ -18,10 +20,10 @@ public class UserService {
         this.userRepo = userRepo;
         this.idService = idService;
     }
-    public List<User> list(){
-        return UserRepo.findAll();}
+   public List<User> list(){
+        return userRepo.findAll();}
     public User findById(String id) {
-        Optional<User> optionalUser = UserRepo.findById(id);
+        Optional<User> optionalUser = userRepo.findById(id);
         if (optionalUser.isPresent()) {
             return optionalUser.get();
         }
